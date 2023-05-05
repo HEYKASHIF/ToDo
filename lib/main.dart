0a7df494f18e488cba75ae2b4784e99a5cae0569
HEYKASHIF <kashifmohd269@gmail.com>
2023-05-05 22:39:39 +0530

    5-may commit8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:to_do/screens/HomePage.dart';
+import 'package:to_do/screens/todoList.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: todoList(),
     );
   }
 }
--- lib/screens/todoList.dart
+++ lib/screens/todoList.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class todoList extends StatelessWidget {
+  const todoList({super.key});
 
   @override
   Widget build(BuildContext context) {
