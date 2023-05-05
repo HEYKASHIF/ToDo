1bb41e8d9c2d82916c44a4644bfb3ad9651ab07a
HEYKASHIF <kashifmohd269@gmail.com>
2023-05-05 22:42:32 +0530

    5-may commit9

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: todoList(),
     );
--- lib/screens/todoList.dart
+++ lib/screens/todoList.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class todoList extends StatelessWidget {
+class todoList extends StatefulWidget {
   const todoList({super.key});
 
+  @override
+  State<todoList> createState() => _todoListState();
+}
+
+class _todoListState extends State<todoList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         centerTitle: true,
         title: Text(
           'To Do',
